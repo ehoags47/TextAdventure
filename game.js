bc8cebd537a45086628129ec535b29152261663b
ehoags47 <Eric.Hogan1@marist.edu>
2014-11-14 10:47:32 -0500

    FINISH

--- game.js
+++ game.js
@@ -1,3 +1,67 @@
+var makePlayer = function(name, maxhealth){
+   var newPlayer = {
+                name: name,
+                maHealth: maxHealth,
+                items: []
+        };
+   newPlayer.pickUp = function(item){
+                this.items.push(item);
+        };
+   newPlayer.drop = function(item){
+                var itemLocation = this.items.indexOf(item);
+                if (itemLocation > 0){
+                        this.items.splice(item, 1);
+                }
+        }
+    newPlayer.toString = function(){
+                return "You have: " + this.items.toString();
+        };
+  return newPlayer;
+}
+
+function interpret(input){
+  console.log(" interpret");
+  var trimmedStr = input.trim();
+  var splitStr = trimmedStr.split();
+  var action = splitStr.shift();
+  var target = splitStr.join();
+}
+function execute(command){
+  console.log(" execute");
+  var action = command.action;
+  var actionFunction = player[action];
+  \actionFunction(.......................);
+}
+function report(){
+  console.log(" report");
+}
+function gameStep(input){
+  console.log("Stepping...");
+  var command = interpret(input);
+  execute(command);
+  report();
+}
+function gameStart(){
+  console.log("Starting...");
+  var inputBox = document.querySelector("input");
+  inputBox.addEventListener("keyup", function(event){
+    if(event.keycode === 13){
+      var inputText = this.value;
+      gameStep(inputText);
+    }
+  });
+}
+
+window.onload = gameStart
+
+
+
+
+
+
+
+
+
 var playing around = funtion(){
   var descripElem = document.getElementById('descrip');
   
